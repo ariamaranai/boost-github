a1c8ede0329aa1444284efb0db0a6d614d149b17
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-20 18:31:37 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -34,4 +34,79 @@ navigator.sendBeacon = () => 0;
         return setAttr.call(this, a, b);
     }
   }
+  let hasOwnProperty = Object.prototype.hasOwnProperty;
+  Object.prototype.hasOwnProperty = function (a) {
+    switch (a) {
+      case "alt":
+      case "aria-activedescendant":
+      case "aria-busy":
+      case "aria-controls":
+      case "aria-current":
+      case "aria-describedby":
+      case "aria-disabled":
+      case "aria-expanded":
+      case "aria-haspopup":
+      case "aria-hidden":
+      case "aria-invalid":
+      case "aria-label":
+      case "aria-labelledby":
+      case "aria-presswed":
+      case "aria-required":
+      case "ariaActiveDescendant":
+      case "ariaAtomic":
+      case "ariaAutoComplete":
+      case "ariaBusy":
+      case "ariaChecked":
+      case "ariaColCount":
+      case "ariaColIndex":
+      case "ariaColSpan":
+      case "ariaControls":
+      case "ariaDescribedBy":
+      case "ariaDetails":
+      case "ariaDisabled":
+      case "ariaErrorMessage":
+      case "ariaExpanded":
+      case "ariaFlowTo":
+      case "ariaGrabbed":
+      case "ariaHasPopup":
+      case "ariaHidden":
+      case "ariaInvalid":
+      case "ariaKeyShortcuts":
+      case "ariaLabel":
+      case "ariaLabelledBy":
+      case "ariaLevel":
+      case "ariaLive":
+      case "ariaModal":
+      case "ariaMultiLine":
+      case "ariaMultiSelectable":
+      case "ariaOrientation":
+      case "ariaOwns":
+      case "ariaPlaceholder":
+      case "ariaPosInSet":
+      case "ariaPressed":
+      case "ariaReadOnly":
+      case "ariaRelevant":
+      case "ariaRequired":
+      case "ariaRoleDescription":
+      case "ariaRowCount":
+      case "ariaRowIndex":
+      case "ariaRowSpan":
+      case "ariaSelected":
+      case "ariaSetSize":
+      case "ariaSort":
+      case "ariaValueMax":
+      case "ariaValueMin":
+      case "ariaValueNow":
+      case "ariaValueText":
+      case "borderRadius":
+      case "role":
+      case "tabIndex":
+      case "textDecoration":
+      case "title":
+        return 0;
+      default:
+        return hasOwnProperty.call(this, a);
+    }
+
+  }
 }
